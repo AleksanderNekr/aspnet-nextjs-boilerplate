a2c494ac41fa8727eeae8b8b1df00d828a414ada
aleks <aleks@noreply.com>
2024-10-07 16:59:58 +0500

    chore(back): setup logging using serilog

--- backend/WebApi/Program.cs
+++ backend/WebApi/Program.cs
@@ -1,9 +1,19 @@
+using System.Net.Mime;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.HttpLogging;
+using Serilog;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.ConfigureHttpJsonOptions(options => options.SerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
+SetupLogging(builder);
+
 var app = builder.Build();
+
+app.UseSerilogRequestLogging();
 
 if (app.Environment.IsDevelopment())
 {
@@ -11,6 +21,29 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+Log.Information("Application started");
 
 app.Run();
+
+return;
+
+static void SetupLogging(WebApplicationBuilder builder)
+{
+    Log.Logger = new LoggerConfiguration()
+        .ReadFrom.Configuration(builder.Configuration)
+        .CreateLogger();
+
+    builder.Host.UseSerilog();
+
+    builder.Services.AddLogging(static builder =>
+    {
+        builder.ClearProviders();
+        builder.AddSerilog(dispose: true);
+    });
+
+    builder.Services.AddHttpLogging(static options =>
+    {
+        options.LoggingFields = HttpLoggingFields.RequestMethod | HttpLoggingFields.Response;
+        options.MediaTypeOptions.AddText(MediaTypeNames.Application.Json);
+    });
+}
